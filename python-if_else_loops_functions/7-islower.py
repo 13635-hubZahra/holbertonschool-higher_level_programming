198fabaefb4e5f0959a06dda4f8000d42860a660
13625-hubZahra <13635@holbertonstudents.com>
2026-04-04 15:11:44 +0000

    ord li task

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-        if ord(c) > 96 and ord(c) < 123:
-            return True
-        else :
-            return False
-c = input("")
-for i in range (len(c)):
-    if islower(cumle[i]):
-        print(f"'{cumle[i]}' balacadi")
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
     else:
-        print(f"'{cumle[i]} boyukdu")
+        return False
