bbc6f60105d5cdd248b514484d19b127ad7a9404
13635-hubZahra <13635@holbertonstudents.com>
2026-04-04 18:53:49 +0000

    salammmmmm

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
     total = 0
-    for i in range (1, len(sys.argv)):
-        total = total + int(sys.argv[i])
+    for i in range(1, count + 1):
+        total += int(sys.argv[i])
     print("{:d}".format(total))
