d4a3cc1a27bb24e1737a9603af743ab3690329df
13635-hubZahra <13635@holbertonstudents.com>
2026-04-04 18:52:13 +0000

    salammm

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+    total = 0
+    for i in range (1, len(sys.argv)):
+        total = total + int(sys.argv[i])
+    print("{:d}".format(total))
