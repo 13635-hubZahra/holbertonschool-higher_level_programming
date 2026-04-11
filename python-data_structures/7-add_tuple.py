f0758a31ec0df526ae67d2f4903aac779c6c8953
13635-hubZahra <13635@holbertonstudents.com>
2026-04-11 11:34:06 +0000

    8-ci taskkkkk

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,17 +5,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         a1 = 0
     if len(tuple_b) >= 1:
-        b1  = tuple_b[0]
+        b1 = tuple_b[0]  
     else:
         b1 = 0
     if len(tuple_a) >= 2:
         a2 = tuple_a[1]
-    else :
+    else:  
         a2 = 0
     if len(tuple_b) >= 2:
         b2 = tuple_b[1]
-    else :
+    else:  
         b2 = 0
     new_1 = a1 + b1
-    new_2  = a2 + b2
-    return(new_1, new_2)
+    new_2 = a2 + b2  
+    return (new_1, new_2) 
