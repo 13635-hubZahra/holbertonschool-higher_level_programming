cd40689647038ff2019cd432e4a0465798ecea33
13635-hubZahra <13635@holbertonstudents.com>
2026-04-22 21:20:20 +0000

    1 ci taskkkk

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+Bu modul verilmis URL-e sorgu gonderir ve
+cavab basligindaki X-Request-Id deyerini gosterir.
+"""
 import urllib.request
-import sys 
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    response.info()
-    print(response.headers.get('X-Request-Id'))
+import sys
+
+
+if __name__ == "__main__":
+    # Skript yalniz birbasa ise salindiqda bura icra olunur
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get('X-Request-Id'))
